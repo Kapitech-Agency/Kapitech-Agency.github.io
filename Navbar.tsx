913a85d14dffb1c1e3e574d7b50d4732f46e8ecb
Kapitech agency <kapitechagency@gmail.com>
2026-04-05 18:09:03 +0700

    Update Navbar.tsx to replace logo path

---
+++ Navbar.tsx
@@ -0,0 +1,14 @@
+// Updated Navbar.tsx content
+
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <img src="./kapitech-logo-white.png" alt="Kapitech Logo" />
+            {/* Other code here */}
+        </nav>
+    );
+};
+
+export default Navbar;
