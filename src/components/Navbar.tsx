91b4087156e35694fb734b28a873a71591420901
Kapitech agency <kapitechagency@gmail.com>
2026-04-05 17:00:00 +0700

    Update logo image source in Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -64,7 +64,7 @@ export const Navbar = () => {
         <div className="max-w-7xl mx-auto px-6 md:px-12 flex items-center justify-between">
           <Link to="/" className="flex items-center group">
             <img 
-              src="/Logo KTA - Blak BG.png" 
+              src="/Kapitech Logo White.png" 
               alt="KAPITECH" 
               className="h-8 md:h-10 w-auto object-contain group-hover:scale-105 transition-transform duration-500"
             />
