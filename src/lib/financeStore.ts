a46b0ede9d56d8213e01c8b62265708266d24cfa
Kapitech agency <kapitechagency@gmail.com>
2026-09-21 22:54:57 +0700

    fix: declare overdue receivables before aggregate

--- src/lib/financeStore.ts
+++ src/lib/financeStore.ts
@@ -727,9 +727,9 @@ export const computeFinancialMetrics = (invoices: AgencyInvoice[], expenses: Age
   const totalApproved = approvedInvoices.reduce((sum, i) => sum + (i.balanceDue !== undefined ? i.balanceDue : i.total), 0);
   const totalSent = sentInvoices.reduce((sum, i) => sum + (i.balanceDue !== undefined ? i.balanceDue : i.total), 0);
   const partialBalance = partiallyPaidInvoices.reduce((sum, i) => sum + (i.balanceDue !== undefined ? i.balanceDue : (i.total - (i.amountPaid || 0))), 0);
+  const totalOverdue = overdueInvoices.reduce((sum, i) => sum + (i.balanceDue !== undefined ? i.balanceDue : i.total), 0);
 
   const totalOutstanding = totalSent + totalApproved + totalOverdue + partialBalance;
-  const totalOverdue = overdueInvoices.reduce((sum, i) => sum + (i.balanceDue !== undefined ? i.balanceDue : i.total), 0);
 
   const opExExpenses = expenses.filter(e => e.type !== 'CapEx').reduce((sum, e) => sum + (Number(e.amount) || 0), 0);
   const capExExpenses = expenses.filter(e => e.type === 'CapEx').reduce((sum, e) => sum + (Number(e.amount) || 0), 0);
