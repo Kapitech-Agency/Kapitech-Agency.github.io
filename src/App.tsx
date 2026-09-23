2b8b049a50a64ec2d0cc01979420e6b6d2dbe43c
Kapitech agency <kapitechagency@gmail.com>
2026-09-23 17:33:34 +0700

    motion: align route transitions with Apple-style hierarchy

--- src/App.tsx
+++ src/App.tsx
@@ -67,7 +67,7 @@ const AnimatedRoutes = () => {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
-        transition={{ duration: 0.35, ease: [0.22, 1, 0.36, 1] }}
+        transition={{ duration: 0.24, ease: [0.22, 1, 0.36, 1] }}
         className="w-full flex-1 flex flex-col"
       >
         <Suspense
