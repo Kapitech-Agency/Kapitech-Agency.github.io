cdb532a80120ce2b09c9dd6eb63c4a12965a1c7c
Kapitech agency <kapitechagency@gmail.com>
2026-09-23 18:22:40 +0700

    fix: remove non-Apple perspective tilt

--- src/components/ui/PerspectiveTilt.tsx
+++ src/components/ui/PerspectiveTilt.tsx
@@ -1,5 +1,4 @@
-import React, { useRef, useState } from 'react';
-import { motion, useSpring, useMotionValue, useTransform } from 'motion/react';
+import React from 'react';
 
 interface PerspectiveTiltProps {
   children: React.ReactNode;
@@ -8,56 +7,12 @@ interface PerspectiveTiltProps {
 }
 
 export const PerspectiveTilt = ({ children, className, onClick }: PerspectiveTiltProps) => {
-  const ref = useRef<HTMLDivElement>(null);
-  
-  const x = useMotionValue(0);
-  const y = useMotionValue(0);
-
-  const mouseXSpring = useSpring(x);
-  const mouseYSpring = useSpring(y);
-
-  const rotateX = useTransform(mouseYSpring, [-0.5, 0.5], ["10deg", "-10deg"]);
-  const rotateY = useTransform(mouseXSpring, [-0.5, 0.5], ["-10deg", "10deg"]);
-
-  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
-    if (!ref.current) return;
-    
-    const rect = ref.current.getBoundingClientRect();
-    
-    const width = rect.width;
-    const height = rect.height;
-    
-    const mouseX = e.clientX - rect.left;
-    const mouseY = e.clientY - rect.top;
-    
-    const xPct = mouseX / width - 0.5;
-    const yPct = mouseY / height - 0.5;
-    
-    x.set(xPct);
-    y.set(yPct);
-  };
-
-  const handleMouseLeave = () => {
-    x.set(0);
-    y.set(0);
-  };
-
   return (
-    <motion.div
-      ref={ref}
-      onMouseMove={handleMouseMove}
-      onMouseLeave={handleMouseLeave}
+    <div
       onClick={onClick}
-      style={{
-        rotateY,
-        rotateX,
-        transformStyle: "preserve-3d",
-      }}
       className={className}
     >
-      <div style={{ transform: "translateZ(50px)", transformStyle: "preserve-3d" }}>
-        {children}
-      </div>
-    </motion.div>
+      {children}
+    </div>
   );
 };
