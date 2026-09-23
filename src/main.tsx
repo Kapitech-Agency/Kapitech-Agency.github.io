28d1daf0314ce2516e71b642b5142b9984ad41aa
Kapitech agency <kapitechagency@gmail.com>
2026-09-24 01:08:09 +0700

    fix(public-ui): load final public design system last

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx';
 import './index.css';
 import './apple-design-final.css';
 import './apple-ui-overhaul.css';
+import './public-apple-system.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
