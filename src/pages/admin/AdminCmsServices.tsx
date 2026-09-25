31e9e5b71a14058c68c9f28beff11255d07ddb87
Kapitech agency <kapitechagency@gmail.com>
2026-09-25 11:04:07 +0700

    polish AMS mobile layout and controls

--- src/pages/admin/AdminCmsServices.tsx
+++ src/pages/admin/AdminCmsServices.tsx
@@ -317,7 +317,7 @@ export const AdminCmsServices: React.FC = () => {
       </div>
 
       {/* Services List Grid */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 w-full">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 sm:gap-4 w-full">
         {filtered.map((item) => (
           <div
             key={item.slug}
